50d80252a3b8adfa2570334583a1aa455b30abee
Jason Mustafa <mustafa.jason@gmail.com>
2020-11-20 03:13:07 -0500

    Initial commit with basic geofencing implementation
    Adds a geofence on app create at Golden Gate bridge

---
+++ app/src/main/java/com/jasonmustafa/covidreminders/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,39 @@
+package com.jasonmustafa.covidreminders
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.android.gms.location.GeofencingEvent
+
+class GeofenceBroadcastReceiver: BroadcastReceiver() {
+    companion object {
+        var notificationId = 1
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+
+        if (geofencingEvent.hasError()) {
+            val errorMessage = "geofence_error_message"
+            // display error
+        } else {
+            geofencingEvent.triggeringGeofences.forEach {
+                val geofence = it.requestId
+
+                val builder = NotificationCompat.Builder(context!!, "channel_01")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentTitle("Test Title")
+                        .setContentText("test content")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                with (NotificationManagerCompat.from(context)) {
+                    notify(notificationId, builder.build())
+                }
+
+                notificationId++
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jasonmustafa/covidreminders/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.jasonmustafa.covidreminders
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import android.util.Log
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.java.simpleName
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+
+    private val location0 = Location("")
+
+    private lateinit var locations: Array<Location>
+
+    @SuppressLint("MissingPermission")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        // golden gate bridge
+        location0.latitude = 37.8199
+        location0.longitude = -122.4783
+
+        println("added location")
+
+        locations = arrayOf(location0)
+
+        val geofences = locations.map {
+            Geofence.Builder()
+                    .setRequestId("home")
+                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                    .setCircularRegion(location0.latitude, location0.longitude, 45F)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER
+                            or Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build()
+        }
+
+        val geofencingRequest = GeofencingRequest.Builder().apply {
+            setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+            addGeofences(geofences)
+        }.build()
+
+        val geofencePendingIntent: PendingIntent by lazy {
+            val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
+            PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        val geofencingClient = LocationServices.getGeofencingClient(this)
+
+        geofencingClient.addGeofences(geofencingRequest, geofencePendingIntent).run {
+            addOnFailureListener {
+                println("failure")
+            }
+
+            addOnSuccessListener {
+                println("success")
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (!checkPermissions()) {
+            requestPermissions()
+        } else {
+           // TODO
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        val permissionState =
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+        return permissionState == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.")
+        } else {
+            Log.i(TAG, "Requesting permission")
+
+            ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "channel_01"
+            val descriptionText = "description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("channel_01", name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
